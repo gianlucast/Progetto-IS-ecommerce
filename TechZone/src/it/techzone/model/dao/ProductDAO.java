aca0682fb915a41d0b82a51598ab76a98f5b2f29
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-05 23:26:41 +0100

    ProductDao e UserDao update con operazioni Obj Design

---
+++ TechZone/src/it/techzone/model/dao/ProductDAO.java
@@ -0,0 +1,193 @@
+package it.techzone.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import it.techzone.model.beans.Product;
+
+
+
+
+
+public class ProductDAO {
+	private static final String PRODUCT_TABLE = "techzone.`PRODUCT`";
+	
+	public synchronized Product retrieveProductById(long id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String selectSQL = "SELECT * FROM " +PRODUCT_TABLE+ " WHERE codiceProdotto = ? AND eliminato=0";
+		Product p=null;
+		try {
+			connection = DriverManagerConnectionPool.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setLong(1, id);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				p.setCodice(id);
+				p.setCategoria(rs.getString("CATEGRIA"));
+				p.setTipo(rs.getString("TIPO"));
+				p.setDescrizione(rs.getString("DESCRIZIONE"));
+				p.setNomeProd(rs.getString("nomeProd"));
+				p.setQuantita(rs.getInt("QUANTITA"));
+				p.setCosto(rs.getFloat("COSTO"));
+				p.setImmagine(rs.getBytes("IMMAGINE"));
+			}
+			return p;
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+	}
+
+	public synchronized boolean doDeleteProduct(long id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "UPDATE " + PRODUCT_TABLE + " SET eliminato = 1 WHERE codiceProdotto= ?";
+
+		try {
+			connection = DriverManagerConnectionPool.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setLong(1, id);
+
+			result = preparedStatement.executeUpdate();
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+		return (result != 0);
+	}
+
+	public synchronized ArrayList<Product> doRetrieveAll(String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ArrayList<Product> products = new ArrayList<Product>();
+
+		String selectSQL = "SELECT * FROM " + PRODUCT_TABLE;
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = DriverManagerConnectionPool.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			ResultSet rs = preparedStatement.executeQuery();
+	
+			while(rs.next()) {
+				Product p=new Product();
+				p.setCodice(rs.getLong("codiceProdotto"));
+				p.setCategoria(rs.getString("CATEGORIA"));
+				p.setTipo(rs.getString("TIPO"));
+				p.setDescrizione(rs.getString("DESCRIZIONE"));
+				p.setNomeProd(rs.getString("nomeProd"));
+				p.setQuantita(rs.getInt("QUANTITA"));
+				p.setCosto(rs.getFloat("COSTO"));
+				p.setImmagine(rs.getBytes("IMMAGINE"));
+				products.add(p);
+			}
+			return products;
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+	}
+
+	public static ArrayList<Product> doRetrieveByCat(String cat) {
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT * FROM "+PRODUCT_TABLE+" WHERE categoria=?");
+			ps.setString(1, cat);
+			ArrayList<Product> prodotti = new ArrayList<Product>();
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Product p=new Product();
+				p.setCodice(rs.getLong("codiceProdotto"));
+				p.setCategoria(rs.getString("CATEGORIA"));
+				p.setTipo(rs.getString("TIPO"));
+				p.setDescrizione(rs.getString("DESCRIZIONE"));
+				p.setNomeProd(rs.getString("nomeProd"));
+				p.setQuantita(rs.getInt("QUANTITA"));
+				p.setCosto(rs.getFloat("COSTO"));
+				p.setImmagine(rs.getBytes("IMMAGINE"));
+				prodotti.add(p);
+			}
+			return prodotti;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static ArrayList<Product> doRetrieveByName(String nameProd) {
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT * FROM "+PRODUCT_TABLE+" WHERE MATCH(nomeProd) AGAINST(? IN BOOLEAN MODE)");
+			ps.setString(1, nameProd);
+			ArrayList<Product> prodotti = new ArrayList<Product>();
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Product p=new Product();
+				p.setCodice(rs.getLong("codiceProdotto"));
+				p.setCategoria(rs.getString("CATEGORIA"));
+				p.setTipo(rs.getString("TIPO"));
+				p.setDescrizione(rs.getString("DESCRIZIONE"));
+				p.setNomeProd(rs.getString("nomeProd"));
+				p.setQuantita(rs.getInt("QUANTITA"));
+				p.setCosto(rs.getFloat("COSTO"));
+				p.setImmagine(rs.getBytes("IMMAGINE"));
+				prodotti.add(p);
+			}
+			return prodotti;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public synchronized void doUpdate(Product prodotto) throws SQLException{
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String insertSQL="UPDATE "+PRODUCT_TABLE+" SET nomeProd=?, descrizione=?, quantita=?, categoria=?, tipo=?, costo=?"
+				+ " WHERE codiceProdotto=?";
+		try {
+			connection = DriverManagerConnectionPool.getConnection();
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, prodotto.getNomeProd());
+			preparedStatement.setString(2, prodotto.getDescrizione());
+			preparedStatement.setInt(3, prodotto.getQuantita());
+			preparedStatement.setString(4, prodotto.getCategoria());
+			preparedStatement.setString(5, prodotto.getTipo());
+			preparedStatement.setFloat(6, prodotto.getCosto());
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+	}
+
+}
--- TechZone/src/it/techzone/model/dao/UserDAO.java
+++ TechZone/src/it/techzone/model/dao/UserDAO.java
@@ -2,8 +2,10 @@ package it.techzone.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import it.techzone.model.beans.Manager;
 import it.techzone.model.beans.UtenteRegistrato;
 import it.techzone.model.dao.DriverManagerConnectionPool;
 
@@ -17,7 +19,7 @@ public class UserDAO {
 		PreparedStatement preparedStatement=null;
 		
 		String query="INSERT INTO "+ UTENTE_TABLE+" "
-				+ "(NOME, COGNOME, TELEFONO, `PASSWORD`, EMAIL, INDIRIZZO, METODOPAGAMENTO) "
+				+ "(NOME, COGNOME, TELEFONO, `PASSWORD`, EMAIL, INDIRIZZO, MetodoPagamento) "
 				+ "VALUES (?,?,?,SHA1(?),?,?,?);";
 		
 		try{
@@ -39,6 +41,112 @@ public class UserDAO {
 					preparedStatement.close();
 			} finally {
 				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+	}
+	
+	public synchronized UtenteRegistrato authenticate(String email, String password) throws SQLException {
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		UtenteRegistrato u=null;
+		String query="SELECT * FROM "+UTENTE_TABLE+" WHERE EMAIL=? AND `PASSWORD`=SHA1(?)";
+		
+		try {
+			connection=DriverManagerConnectionPool.getConnection();
+			preparedStatement=connection.prepareStatement(query);
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			
+			ResultSet rs=preparedStatement.executeQuery();
+			if(rs.next()) {
+				u=new UtenteRegistrato();
+				u.setId(rs.getLong("ID"));
+				u.setCognome(rs.getString("COGNOME"));
+				u.setNome(rs.getString("NOME"));
+				u.setEmail(email);
+				u.setPassword(rs.getString("PASSWORD"));
+				u.setIndirizzo(rs.getString("INDIRIZZO"));
+				u.setPagamento(rs.getString("metodoPagamento"));
+				u.setTelefono(rs.getLong("TELEFONO"));
+			}
+			return u;
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+		
+	}
+	
+	public synchronized Manager authenticateManager(String email, String password) throws SQLException {
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		Manager u=null;
+		String query="SELECT * FROM "+MANAGER_TABLE+" WHERE EMAIL=? AND `PASSWORD`=SHA1(?)";
+		
+		try {
+			connection=DriverManagerConnectionPool.getConnection();
+			preparedStatement=connection.prepareStatement(query);
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			
+			ResultSet rs=preparedStatement.executeQuery();
+			if(rs.next()) {
+				u=new Manager();
+				u.setId(rs.getLong("ID"));
+				u.setCognome(rs.getString("COGNOME"));
+				u.setNome(rs.getString("NOME"));
+				u.setEmail(email);
+				u.setPassword(rs.getString("PASSWORD"));
+				u.setSupportEmail(rs.getString("SUPPORTEMAIL"));
+				u.setTelefono(rs.getLong("TELEFONO"));
+			}
+			return u;
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+		
+	}
+	
+	public synchronized boolean checkEmailUsed(String email) throws SQLException {
+		Connection connection=null;
+		Connection connection2=null;
+		PreparedStatement preparedStatement=null;
+		PreparedStatement preparedStatement2 = null;
+		ResultSet rs;
+		String query1="SELECT * FROM "+MANAGER_TABLE+" WHERE EMAIL=?";
+		String query2="SELECT * FROM "+UTENTE_TABLE+" WHERE EMAIL=?";
+		
+		try {
+			connection=DriverManagerConnectionPool.getConnection();
+			preparedStatement=connection.prepareStatement(query1);
+			preparedStatement.setString(1, email);
+			rs=preparedStatement.executeQuery();
+			if(rs.next()) return true;
+			
+			connection2=DriverManagerConnectionPool.getConnection();
+			preparedStatement2=connection.prepareStatement(query2);
+			preparedStatement2.setString(1, email);
+			rs=preparedStatement.executeQuery();
+			if(rs.next()) return true;
+			
+			return false;
+		}finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (preparedStatement2 != null)
+					preparedStatement2.close();
+			} finally {
+				DriverManagerConnectionPool.releaseConnection(connection2);
 			}
 		}
 	}
