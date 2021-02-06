18eecb275f5c6618f6395bc8a71e891e019a5039
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-06 18:43:10 +0100

    Ritorno a cart vecchio

---
+++ TechZone/src/it/techzone/model/models/CartManager.java
@@ -0,0 +1,45 @@
+package it.techzone.model.models;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import it.techzone.model.beans.*;
+import it.techzone.model.dao.*;
+
+public class CartManager {
+	private ProductDAO productdao;
+	
+	public boolean cartExists(HttpSession session) {
+		if(session.getAttribute("cart")==null)return false;
+		return true;
+	}
+	
+	public Cart retrieveCart(HttpSession session) {
+		if(cartExists(session)==false)return null;
+		else return (Cart)session.getAttribute("cart");
+	}
+	
+	public Cart newCart(HttpSession session) {
+		Cart c=new Cart();
+		session.setAttribute("cart", c);
+		return c;
+	}
+	
+	public boolean checkQuantity(long idProd, int quantita, HttpSession session) throws SQLException {
+		productdao=new ProductDAO();
+		Product p=productdao.retrieveProductById(idProd);
+		if(session.getAttribute("cart")==null) newCart(session);
+		Cart c=(Cart)session.getAttribute("cart");
+		int pos=c.isInCart(p);
+		int quantitadesiderata=quantita;
+		if(pos>0) quantitadesiderata+=c.getProductList().get(pos).getQuantita();
+		
+		if(quantitadesiderata>p.getQuantita()) return false;
+		return true;
+	}
+	
+	public boolean modCart(long idProd, int quantità, HttpSession sessione) {
+		return false;
+	}
+}
