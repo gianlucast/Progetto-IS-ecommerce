3864d843bfd0e306552fd21059c6d8fb55d41bf4
emanu <emanu@DESKTOP-2KA5IGG>
2021-02-07 19:42:48 +0100

    update product

---
+++ TechZone/src/it/techzone/control/UpdateProductControl.java
@@ -0,0 +1,43 @@
+package it.techzone.control;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import it.techzone.model.beans.Product;
+import it.techzone.model.models.ProductManager;
+
+public class UpdateProductControl {
+ static ProductManager pm = new ProductManager();
+ 
+ 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+ 		HttpSession session = request.getSession();
+ 		try {
+ 			if(session.getAttribute("manager")!=null) {
+ 			
+ 				String categoria= request.getParameter("categoria");
+ 				String codice= request.getParameter("codice");
+ 				long code = Long.parseLong(codice);
+ 				String costo= request.getParameter("costo");
+ 				float cost= Float.parseFloat(costo);
+ 				String descrizione = request.getParameter("descrizione");
+ 				String nomeProd = request.getParameter("nomeprod");
+ 				String quantità = request.getParameter("quantità");
+ 				int quantity = Integer.parseInt(quantità);
+ 				String tipo= request.getParameter("tipo");
+ 			
+ 				pm.updateProduct(code, descrizione, nomeProd, quantity, categoria, tipo, cost);
+ 				session.setAttribute("alertMsg", "Modifica effettuata");
+ 				response.sendRedirect("./Productmanagement.jsp");
+ 			}
+ 			else {
+ 				session.setAttribute("alertMsg", "Accesso non autorizzato");
+ 				response.sendRedirect("./Homepage.jsp");
+ 			}
+ 		}
+ 		
+ 		catch(Exception e2) {System.out.println(e2);}
+ 		
+ 	
+ 	}
+}
