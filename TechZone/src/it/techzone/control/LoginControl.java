24e545b858dcffd3e13eed16a25abaf6d9967a8f
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-07 13:27:45 +0100

    Aggiunto loginControl

---
+++ TechZone/src/it/techzone/control/LoginControl.java
@@ -0,0 +1,38 @@
+package it.techzone.control;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.techzone.model.beans.Manager;
+import it.techzone.model.beans.UtenteRegistrato;
+import it.techzone.model.models.UserManager;
+
+public class LoginControl extends HttpServlet{
+	static UserManager um=new UserManager();
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		HttpSession session=request.getSession();
+		
+		try {
+			UtenteRegistrato u=um.authentication(email,password);
+			if(u==null) {
+				Manager m=um.authenticationManager(email, password);
+				if(m==null) { 
+					session.setAttribute("alertMsg", "Corrispondenza non trovata");
+					response.sendRedirect("./Login.jsp");
+				}
+				else {
+					session.setAttribute("manager", m);
+				}
+			}else 
+				session.setAttribute("utente", u);
+			session.setAttribute("alertMsg", "Login avvenuto con successo");
+			response.sendRedirect("./Homepage.jsp");
+			
+		}catch (Exception e2) {System.out.println(e2);} 
+	}
+}
