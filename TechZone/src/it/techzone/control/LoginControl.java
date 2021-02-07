e57aee572e7119f120c994d5946d73c918f7b515
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-07 18:41:24 +0100

    LoginControl update

--- TechZone/src/it/techzone/control/LoginControl.java
+++ TechZone/src/it/techzone/control/LoginControl.java
@@ -18,20 +18,25 @@ public class LoginControl extends HttpServlet{
 		HttpSession session=request.getSession();
 		
 		try {
-			UtenteRegistrato u=um.authentication(email,password);
-			if(u==null) {
-				Manager m=um.authenticationManager(email, password);
-				if(m==null) {   
-					session.setAttribute("alertMsg", "Corrispondenza non trovata");
-					response.sendRedirect("./Login.jsp");
-				}
-				else {
-					session.setAttribute("manager", m);
-				}
-			}else 
-				session.setAttribute("utente", u);
-			session.setAttribute("alertMsg", "Login avvenuto con successo");
-			response.sendRedirect("./Homepage.jsp");
+			if(session.getAttribute("utente")!=null||session.getAttribute("manager")!=null) {
+				session.setAttribute("alertMsg", "Hai già effettuato il login");
+				response.sendRedirect("./Homepage.jsp");
+			}else {
+				UtenteRegistrato u=um.authentication(email,password);
+				if(u==null) {
+					Manager m=um.authenticationManager(email, password);
+					if(m==null) {   
+						session.setAttribute("alertMsg", "Corrispondenza non trovata");
+						response.sendRedirect("./Login.jsp");
+					}
+					else {
+						session.setAttribute("manager", m);
+					}
+				}else 
+					session.setAttribute("utente", u);
+				session.setAttribute("alertMsg", "Login avvenuto con successo");
+				response.sendRedirect("./Homepage.jsp");
+			}
 			
 		}catch (Exception e2) {System.out.println(e2);} 
 	}
