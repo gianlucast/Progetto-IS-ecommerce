f774e2ceb0faeff368c114135c27458f9a2d0816
emanu <emanu@DESKTOP-2KA5IGG>
2021-02-09 18:17:28 +0100

    push prova

--- TechZone/src/it/techzone/control/ProductCatalogueControl.java
+++ TechZone/src/it/techzone/control/ProductCatalogueControl.java
@@ -42,7 +42,7 @@ public class ProductCatalogueControl extends HttpServlet{
 		
 			catch (SQLException | ServletException | IOException e) {
 				e.printStackTrace();
-				System.out.println("");
+				
 			}
 		}
 }
