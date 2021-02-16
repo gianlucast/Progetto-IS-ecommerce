214d00885c80618e3229895cb1f3d3c3a7fdbdae
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-16 18:40:32 +0100

    librerie per mockito

---
+++ TechZone/src/it/techzone/test/control/LoginControlTest.java
@@ -0,0 +1,8 @@
+package it.techzone.test.control;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+public class LoginControlTest {
+
+}
