f88cc7c5777ae953c12c080110972ad15aa62a78
GerardoFesta <g.festa22@studenti.unisa.it>
2021-02-17 23:05:16 +0100

    LoginControlTest update1

--- TechZone/src/it/techzone/test/control/LoginControlTest.java
+++ TechZone/src/it/techzone/test/control/LoginControlTest.java
@@ -1,5 +1,6 @@
 package it.techzone.test.control;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import it.techzone.control.LoginControl;
+import it.techzone.model.beans.Manager;
+import it.techzone.model.beans.UtenteRegistrato;
 import junit.framework.TestCase;
 public class LoginControlTest extends Mockito{
 	@Mock
@@ -36,11 +39,11 @@ public class LoginControlTest extends Mockito{
         request=mock(HttpServletRequest.class);
         response=mock(HttpServletResponse.class);
         session=mock(HttpSession.class);
-        sessioneMap=new HashMap<String, Object>();
     }
     
     @Test
     public void testLogin() {
+    	sessioneMap=new HashMap<String, Object>();
     	boolean flag=false;
     	try {
 	    	when(request.getSession()).thenReturn(session);
@@ -76,6 +79,10 @@ public class LoginControlTest extends Mockito{
 	        assertEquals("./HomePage.jsp", captor.getValue());
 	        
 	    	assertEquals("Login avvenuto con successo", sessioneMap.get("alertMsg"));
+	    	assertNotEquals(sessioneMap.get("utente"),null);
+	    	assertEquals(sessioneMap.get("manager"),null);
+	    
+	    	
 	    	flag=true;
 	    
     	}catch(Exception e) {
@@ -84,5 +91,199 @@ public class LoginControlTest extends Mockito{
 		}finally {
             if(flag) System.out.println("\tResult: "+Thread.currentThread().getStackTrace()[1].getMethodName()+" passed!");
         }
+    }
+    	
+    @Test
+    public void testLoginManager() {
+    	sessioneMap=new HashMap<String, Object>();
+    	boolean flag=false;
+    	try {
+	    	when(request.getSession()).thenReturn(session);
+	    	when(request.getParameter("email")).thenReturn("giuseppe@techzone.it");
+	    	when(request.getParameter("password")).thenReturn("Amministratore1");
+	    	when(session.getAttribute("utente")).thenReturn(null);
+	    	when(session.getAttribute("manager")).thenReturn(null);
+	    	
+	    	doAnswer(new Answer<Object>() {
+	    		@Override
+	    		public Object answer(InvocationOnMock invocation)throws Throwable{
+	    			String key= (String) invocation.getArguments()[0];
+	    			return sessioneMap.get(key);
+	    		}
+	    	}).when(session).getAttribute(anyString());
+	    	
+	    	Mockito.doAnswer(new Answer<Object>(){
+	            @Override
+	            public Object answer(InvocationOnMock invocation) throws Throwable {
+	                String key = (String) invocation.getArguments()[0];
+	                Object value = invocation.getArguments()[1];
+	                sessioneMap.put(key, value);
+	                return null;
+	            }
+	        }).when(session).setAttribute(anyString(), any());
+	    	
+	    	
+	    	ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+	        
+	    	new LoginControl().doGet(request,response);
+	    	
+	    	verify(response).sendRedirect(captor.capture());
+	        assertEquals("./HomePage.jsp", captor.getValue());
+	        
+	    	assertEquals("Login avvenuto con successo", sessioneMap.get("alertMsg"));
+	    	
+	    	
+	    	assertNotEquals(sessioneMap.get("manager"),null);
+	    	assertEquals(sessioneMap.get("utente"),null);
+	    	
+	    	flag=true;
+	    
+    	}catch(Exception e) {
+			e.printStackTrace();
+			fail("testLoginManager() not passed!");
+		}finally {
+            if(flag) System.out.println("\tResult: "+Thread.currentThread().getStackTrace()[1].getMethodName()+" passed!");
+        }
+    }
+    
+    @Test
+    public void testLoginFail() {
+    	boolean flag1=false;
+    	boolean flag2=false;
+    	boolean flag3=false;
+    	try {
+    		sessioneMap=new HashMap<String, Object>();
+	    	when(request.getSession()).thenReturn(session);
+	    	when(request.getParameter("email")).thenReturn("giuseppe@techzone.it");
+	    	when(request.getParameter("password")).thenReturn("Amministratore1");
+	    	sessioneMap.put("utente", new UtenteRegistrato());
+	    	when(session.getAttribute("utente")).thenReturn(null);
+	    	when(session.getAttribute("manager")).thenReturn(null);
+	    	
+	    	doAnswer(new Answer<Object>() {
+	    		@Override
+	    		public Object answer(InvocationOnMock invocation)throws Throwable{
+	    			String key= (String) invocation.getArguments()[0];
+	    			return sessioneMap.get(key);
+	    		}
+	    	}).when(session).getAttribute(anyString());
+	    	
+	    	Mockito.doAnswer(new Answer<Object>(){
+	            @Override
+	            public Object answer(InvocationOnMock invocation) throws Throwable {
+	                String key = (String) invocation.getArguments()[0];
+	                Object value = invocation.getArguments()[1];
+	                sessioneMap.put(key, value);
+	                return null;
+	            }
+	        }).when(session).setAttribute(anyString(), any());
+	    	
+	    	
+	    	ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+	        
+	    	new LoginControl().doGet(request,response);
+	    	
+	    	verify(response).sendRedirect(captor.capture());
+	    	assertEquals("Hai già effettuato il login", sessioneMap.get("alertMsg"));
+	    	assertEquals("./HomePage.jsp", captor.getValue());
+	        assertEquals(null, sessioneMap.get("manager"));
+	        flag1=true;
+	    	
+	    	
+	    	
+	    
+    	}catch(Exception e) {
+    		fail("testLoginFail() (utente already logged) not passed!");
+			
+		}
+    	
+    	try {
+    		sessioneMap=new HashMap<String, Object>();
+	    	when(request.getSession()).thenReturn(session);
+	    	when(request.getParameter("email")).thenReturn("giuseppe@techzone.it");
+	    	when(request.getParameter("password")).thenReturn("Amministratore1");
+	    	sessioneMap.put("manager", new Manager());
+	    	when(session.getAttribute("utente")).thenReturn(null);
+	    	when(session.getAttribute("manager")).thenReturn(null);
+	    	
+	    	doAnswer(new Answer<Object>() {
+	    		@Override
+	    		public Object answer(InvocationOnMock invocation)throws Throwable{
+	    			String key= (String) invocation.getArguments()[0];
+	    			return sessioneMap.get(key);
+	    		}
+	    	}).when(session).getAttribute(anyString());
+	    	
+	    	Mockito.doAnswer(new Answer<Object>(){
+	            @Override
+	            public Object answer(InvocationOnMock invocation) throws Throwable {
+	                String key = (String) invocation.getArguments()[0];
+	                Object value = invocation.getArguments()[1];
+	                sessioneMap.put(key, value);
+	                return null;
+	            }
+	        }).when(session).setAttribute(anyString(), any());
+	    	
+	    	
+	    	ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+	        
+	    	new LoginControl().doGet(request,response);
+	    	
+	    	assertEquals("Hai già effettuato il login", sessioneMap.get("alertMsg"));
+	        assertEquals(null, sessioneMap.get("utente"));
+	        flag2=true;
+	    	
+	    	
+	    	
+	    
+    	}catch(Exception e) {
+    		fail("testLoginFail() (manager already logged) not passed!");
+			
+		}
+    	
+    	try {
+    		sessioneMap=new HashMap<String, Object>();
+	    	when(request.getSession()).thenReturn(session);
+	    	when(request.getParameter("email")).thenReturn("giuseppe@techzone.it");
+	    	when(request.getParameter("password")).thenReturn("NonGiustaPassword");
+	    	when(session.getAttribute("utente")).thenReturn(null);
+	    	when(session.getAttribute("manager")).thenReturn(null);
+	    	
+	    	doAnswer(new Answer<Object>() {
+	    		@Override
+	    		public Object answer(InvocationOnMock invocation)throws Throwable{
+	    			String key= (String) invocation.getArguments()[0];
+	    			return sessioneMap.get(key);
+	    		}
+	    	}).when(session).getAttribute(anyString());
+	    	
+	    	Mockito.doAnswer(new Answer<Object>(){
+	            @Override
+	            public Object answer(InvocationOnMock invocation) throws Throwable {
+	                String key = (String) invocation.getArguments()[0];
+	                Object value = invocation.getArguments()[1];
+	                sessioneMap.put(key, value);
+	                return null;
+	            }
+	        }).when(session).setAttribute(anyString(), any());
+	    	
+	    	
+	    	ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+	        
+	    	new LoginControl().doGet(request,response);
+	    	
+	    	assertEquals(null,sessioneMap.get("utente"));
+	    	assertEquals(null,sessioneMap.get("manager"));
+	    	assertEquals("Corrispondenza non trovata", sessioneMap.get("alertMsg"));
+	        
+	    	flag3=true;
+	    	
+	    	
+	    
+    	}catch(Exception e) {
+    		fail("testLoginFail() (manager already logged) not passed!");
+			
+		}
+    	if(flag1&&flag2&&flag3) System.out.println("\tResult: "+Thread.currentThread().getStackTrace()[1].getMethodName()+" passed!");
     }
 }
